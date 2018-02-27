f3ed5629a6892d81295920bee5868bb7d5380b6a
Andre Linoge <andrelinoge@gmail.com>
2018-02-27 12:47:12 -0800

    changed admin theme

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -1,13 +1,9 @@
-//= require ./plugins/jquery/jquery.min.js
-//= require ./plugins/bootstrap/js/popper.min.js
+//= require ./theme/vendors/vendors.min
+//= require ./theme/core/app
+//= require ./theme/core/app-menu
+//= require ./theme/vendors/ui/jquery.sticky.js
+//= require ./theme/vendors/forms/select/select2.full
 
-//= require ./plugins/bootstrap/js/bootstrap.min.js
-//= require ./jquery.slimscroll.js
-//= require ./waves.js
-//= require ./sidebarmenu.js
-
-//= require ./plugins/sticky-kit-master/dist/sticky-kit.min.js
-//= require ./custom.min.js
-
-//= require ckeditor/init
 //= require rails-ujs
+//= require bootstrap-notify
+//= require moment
