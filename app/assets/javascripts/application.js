00cc27c3e6ff53d6ed03dc18800f24b2495d4cfe
Andre Linoge <andrelinoge@gmail.com>
2018-02-28 12:24:13 -0800

    added main theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,11 @@
+//= require "./theme/jquery-3.2.1.js"
+//= require "./theme/popper.min.js"
+//= require "./theme/bootstrap.min.js"
+//= require "./theme/owl.carousel.js"
+//= require "./theme/jquery.magnific-popup.min.js"
+//= require "./theme/magnific-popup-options.js"
+//= require "./theme/revolution-slider/jquery.themepunch.revolution.min.js"
+//= require "./theme/revolution-slider/jquery.themepunch.tools.min.js"
+//= require "./theme/jquery.fancybox.min.js"
+//= require "./theme/custom.js"
 //= require rails-ujs
