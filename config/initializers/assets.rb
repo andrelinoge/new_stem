40e7c70618b6699fc45738ea50aade546032720b
Andre Linoge <andrelinoge87@gmail.com>
2018-03-01 09:52:27 +0200

    fixing assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( ckeditor/*)
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|gif)\z/
