f0feb85fb1f43347965a76156d0ef1b0c35cb96b
Andre Linoge <andrelinoge@gmail.com>
2018-03-18 13:17:51 -0700

    wip

--- config/puma.rb
+++ config/puma.rb
@@ -40,12 +40,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #   ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
 # end
 
-# The code in the `on_worker_boot` will be called if you are using
-# clustered mode by specifying a number of `workers`. After each worker
-# process is booted, this block will be run. If you are using the `preload_app!`
-# option, you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, as Ruby
-# cannot share connections between processes.
 #
 # on_worker_boot do
 #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
